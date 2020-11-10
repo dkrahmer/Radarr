84f07496ae3c25c7634c1771864b3b9c7ddc5eb2
Doug Krahmer <doug.git@remhark.com>
2020-11-10 10:29:53 -0800

    Remove DiskSpaceConnector to avoid build error

--- frontend/src/System/Status/Status.js
+++ frontend/src/System/Status/Status.js
@@ -3,7 +3,6 @@ import PageContent from 'Components/Page/PageContent';
 import PageContentBody from 'Components/Page/PageContentBody';
 import translate from 'Utilities/String/translate';
 import AboutConnector from './About/AboutConnector';
-import DiskSpaceConnector from './DiskSpace/DiskSpaceConnector';
 import HealthConnector from './Health/HealthConnector';
 import MoreInfo from './MoreInfo/MoreInfo';
 
