dkrahmer/Radarr

commit bd1844030d8af2941af234391cfe5117c194ccc5

New: Rework List sync interval logic
* New: Rework List sync interval logic

(cherry picked from commit c522cd120d08757e7e43c2348be4d7f05a254fac)

* Minor alignment with Sonarr

* Remove ListUpdateInterval

---------

Co-authored-by: Qstick <qstick@gmail.com>

frontend/src/Settings/ImportLists/ImportLists/EditImportListModalContent.js
@@ -14,6 +14,7 @@ import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { inputTypes, kinds } from 'Helpers/Props';
+import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
 import translate from 'Utilities/String/translate';
 import styles from './EditImportListModalContent.css';
 
@@ -42,6 +43,7 @@ function EditImportListModalContent(props) {
     name,
     enabled,
     enableAuto,
+    minRefreshInterval,
     monitor,
     minimumAvailability,
     qualityProfileId,
@@ -85,6 +87,14 @@ function EditImportListModalContent(props) {
                     {message.value.message}
                   </Alert>
               }
+
+              <Alert
+                kind={kinds.INFO}
+                className={styles.message}
+              >
+                {translate('ListWillRefreshEveryInterp', [formatShortTimeSpan(minRefreshInterval.value)])}
+              </Alert>
+
               <FormGroup>
                 <FormLabel>{translate('Name')}</FormLabel>
 

frontend/src/Settings/ImportLists/ImportLists/ImportList.js
@@ -4,6 +4,7 @@ import Card from 'Components/Card';
 import Label from 'Components/Label';
 import ConfirmModal from 'Components/Modal/ConfirmModal';
 import { kinds } from 'Helpers/Props';
+import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
 import translate from 'Utilities/String/translate';
 import EditImportListModalConnector from './EditImportListModalConnector';
 import styles from './ImportList.css';
@@ -56,7 +57,8 @@ class ImportList extends Component {
       id,
       name,
       enabled,
-      enableAuto
+      enableAuto,
+      minRefreshInterval
     } = this.props;
 
     return (
@@ -96,6 +98,12 @@ class ImportList extends Component {
           }
         </div>
 
+        <div className={styles.enabled}>
+          <Label kind={kinds.INFO} title='List Refresh Interval'>
+            {`${translate('Refresh')}: ${formatShortTimeSpan(minRefreshInterval)}`}
+          </Label>
+        </div>
+
         <EditImportListModalConnector
           id={id}
           isOpen={this.state.isEditImportListModalOpen}
@@ -122,6 +130,7 @@ ImportList.propTypes = {
   name: PropTypes.string.isRequired,
   enabled: PropTypes.bool.isRequired,
   enableAuto: PropTypes.bool.isRequired,
+  minRefreshInterval: PropTypes.string.isRequired,
   onConfirmDeleteImportList: PropTypes.func.isRequired
 };
 

frontend/src/Settings/ImportLists/Options/ImportListOptions.js
@@ -50,23 +50,6 @@ function ImportListOptions(props) {
                 advancedSettings={advancedSettings}
                 isAdvanced={true}
               >
-                <FormLabel>{translate('ListUpdateInterval')}</FormLabel>
-
-                <FormInputGroup
-                  type={inputTypes.NUMBER}
-                  name="importListSyncInterval"
-                  min={6}
-                  unit="hours"
-                  helpText={translate('ImportListSyncIntervalHelpText')}
-                  onChange={onInputChange}
-                  {...settings.importListSyncInterval}
-                />
-              </FormGroup>
-
-              <FormGroup
-                advancedSettings={advancedSettings}
-                isAdvanced={true}
-              >
                 <FormLabel>{translate('CleanLibraryLevel')}</FormLabel>
 
                 <FormInputGroup

src/NzbDrone.Core/Configuration/ConfigService.cs
@@ -116,13 +116,6 @@ namespace NzbDrone.Core.Configuration
             set { SetValue("AvailabilityDelay", value); }
         }
 
-        public int ImportListSyncInterval
-        {
-            get { return GetValueInt("ImportListSyncInterval", 24); }
-
-            set { SetValue("ImportListSyncInterval", value); }
-        }
-
         public string ListSyncLevel
         {
             get { return GetValue("ListSyncLevel", "disabled"); }

src/NzbDrone.Core/Configuration/IConfigService.cs
@@ -60,7 +60,6 @@ namespace NzbDrone.Core.Configuration
         bool AllowHardcodedSubs { get; set; }
         string WhitelistedHardcodedSubs { get; set; }
 
-        int ImportListSyncInterval { get; set; }
         string ListSyncLevel { get; set; }
         string ImportExclusions { get; set; }
 

src/NzbDrone.Core/ImportLists/FetchAndParseImportListService.cs
@@ -66,6 +66,15 @@ namespace NzbDrone.Core.ImportLists
                 _logger.ProgressInfo("Syncing Movies for List: {0}", importList.Name);
 
                 var importListLocal = importList;
+
+                var importListStatus = _importListStatusService.GetLastSyncListInfo(importListLocal.Definition.Id);
+
+                if (importListStatus.HasValue && DateTime.UtcNow < importListStatus + importListLocal.MinRefreshInterval)
+                {
+                    _logger.Trace("Skipping refresh of Import List {0} due to minimum refresh inverval", importListLocal.Definition.Name);
+                    continue;
+                }
+
                 var blockedLists = _importListStatusService.GetBlockedProviders().ToDictionary(v => v.ProviderId, v => v);
 
                 if (blockedLists.TryGetValue(importList.Definition.Id, out var blockedListStatus))
@@ -88,7 +97,7 @@ namespace NzbDrone.Core.ImportLists
                             if (!importListReports.AnyFailure)
                             {
                                 var alreadyMapped = result.Movies.Where(x => importListReports.Movies.Any(r => r.TmdbId == x.TmdbId));
-                                var listMovies = MapMovieReports(importListReports.Movies.Where(x => !result.Movies.Any(r => r.TmdbId == x.TmdbId)).ToList()).Where(x => x.TmdbId > 0).ToList();
+                                var listMovies = MapMovieReports(importListReports.Movies.Where(x => result.Movies.All(r => r.TmdbId != x.TmdbId)).ToList()).Where(x => x.TmdbId > 0).ToList();
 
                                 listMovies.AddRange(alreadyMapped);
                                 listMovies = listMovies.DistinctBy(x => x.TmdbId).ToList();
@@ -99,6 +108,8 @@ namespace NzbDrone.Core.ImportLists
                             }
 
                             result.AnyFailure |= importListReports.AnyFailure;
+
+                            _importListStatusService.UpdateListSyncStatus(importList.Definition.Id);
                         }
                     }
                     catch (Exception e)
@@ -111,6 +122,8 @@ namespace NzbDrone.Core.ImportLists
             }
 
             Task.WaitAll(taskList.ToArray());
+
+            result.Movies = result.Movies.DistinctBy(r => new { r.TmdbId, r.ImdbId, r.Title }).ToList();
 
             _logger.Debug("Found {0} reports for all lists", result.Movies.Count);
 
@@ -151,12 +164,16 @@ namespace NzbDrone.Core.ImportLists
                     }
 
                     result.AnyFailure |= importListReports.AnyFailure;
+
+                    _importListStatusService.UpdateListSyncStatus(importList.Definition.Id);
                 }
             }
             catch (Exception e)
             {
                 _logger.Error(e, "Error during Import List Sync for list {0}", importList.Name);
             }
+
+            result.Movies = result.Movies.DistinctBy(r => new { r.TmdbId, r.ImdbId, r.Title }).ToList();
 
             _logger.Debug("Found {0} reports for list {1}", result.Movies.Count, importList.Name);
 
@@ -166,9 +183,9 @@ namespace NzbDrone.Core.ImportLists
         private List<ImportListMovie> MapMovieReports(List<ImportListMovie> reports)
         {
             var mappedMovies = reports.Select(m => _movieSearch.MapMovieToTmdbMovie(new MovieMetadata { Title = m.Title, TmdbId = m.TmdbId, ImdbId = m.ImdbId, Year = m.Year }))
-                                      .Where(x => x != null)
-                                      .DistinctBy(x => x.TmdbId)
-                                      .ToList();
+                .Where(x => x != null)
+                .DistinctBy(x => x.TmdbId)
+                .ToList();
 
             _movieMetadataService.UpsertMany(mappedMovies);
 

src/NzbDrone.Core/Jobs/TaskManager.cs
@@ -121,7 +121,7 @@ namespace NzbDrone.Core.Jobs
 
                     new ScheduledTask
                     {
-                        Interval = GetImportListSyncInterval(),
+                        Interval = 5,
                         TypeName = typeof(ImportListSyncCommand).FullName
                     },
 
@@ -210,14 +210,6 @@ namespace NzbDrone.Core.Jobs
             return interval;
         }
 
-        private int GetImportListSyncInterval()
-        {
-            // Enforce 6 hour min on list sync
-            var interval = Math.Max(_configService.ImportListSyncInterval, 6);
-
-            return interval * 60;
-        }
-
         public void Handle(CommandExecutedEvent message)
         {
             var scheduledTask = _scheduledTaskRepository.All().SingleOrDefault(c => c.TypeName == message.Command.Body.GetType().FullName);
@@ -239,19 +231,15 @@ namespace NzbDrone.Core.Jobs
             var rss = _scheduledTaskRepository.GetDefinition(typeof(RssSyncCommand));
             rss.Interval = GetRssSyncInterval();
 
-            var importList = _scheduledTaskRepository.GetDefinition(typeof(ImportListSyncCommand));
-            importList.Interval = GetImportListSyncInterval();
-
             var backup = _scheduledTaskRepository.GetDefinition(typeof(BackupCommand));
             backup.Interval = GetBackupInterval();
 
             var refreshMonitoredDownloads = _scheduledTaskRepository.GetDefinition(typeof(RefreshMonitoredDownloadsCommand));
             refreshMonitoredDownloads.Interval = GetRefreshMonitoredInterval();
 
-            _scheduledTaskRepository.UpdateMany(new List<ScheduledTask> { rss, importList, refreshMonitoredDownloads, backup });
+            _scheduledTaskRepository.UpdateMany(new List<ScheduledTask> { rss, refreshMonitoredDownloads, backup });
 
             _cache.Find(rss.TypeName).Interval = rss.Interval;
-            _cache.Find(importList.TypeName).Interval = importList.Interval;
             _cache.Find(backup.TypeName).Interval = backup.Interval;
             _cache.Find(refreshMonitoredDownloads.TypeName).Interval = refreshMonitoredDownloads.Interval;
         }

frontend/src/Settings/ImportLists/ImportLists/EditImportListModalContent.css
@@ -3,3 +3,9 @@
 
   margin-right: auto;
 }
+
+.message {
+  composes: alert from '~Components/Alert.css';
+
+  margin-bottom: 30px;
+}

commit 8cb2cb4ccd589c586b291601962f6837bd7ab7f9

Fix List sync interval logic to allow manual list refreshes Broken from PR #8573 Using hardcoded minutes value since the column was removed from the DB: ConfigService.ImportListSyncInterval

frontend/src/Settings/ImportLists/ImportLists/EditImportListModalContent.js
@@ -14,7 +14,6 @@ import ModalContent from 'Components/Modal/ModalContent';
 import ModalFooter from 'Components/Modal/ModalFooter';
 import ModalHeader from 'Components/Modal/ModalHeader';
 import { inputTypes, kinds } from 'Helpers/Props';
-import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
 import translate from 'Utilities/String/translate';
 import styles from './EditImportListModalContent.css';
 
@@ -43,7 +42,6 @@ function EditImportListModalContent(props) {
     name,
     enabled,
     enableAuto,
-    minRefreshInterval,
     monitor,
     minimumAvailability,
     qualityProfileId,
@@ -87,13 +85,6 @@ function EditImportListModalContent(props) {
                     {message.value.message}
                   </Alert>
               }
-
-              <Alert
-                kind={kinds.INFO}
-                className={styles.message}
-              >
-                {translate('ListWillRefreshEveryInterp', [formatShortTimeSpan(minRefreshInterval.value)])}
-              </Alert>
 
               <FormGroup>
                 <FormLabel>{translate('Name')}</FormLabel>

frontend/src/Settings/ImportLists/ImportLists/ImportList.js
@@ -4,7 +4,6 @@ import Card from 'Components/Card';
 import Label from 'Components/Label';
 import ConfirmModal from 'Components/Modal/ConfirmModal';
 import { kinds } from 'Helpers/Props';
-import formatShortTimeSpan from 'Utilities/Date/formatShortTimeSpan';
 import translate from 'Utilities/String/translate';
 import EditImportListModalConnector from './EditImportListModalConnector';
 import styles from './ImportList.css';
@@ -57,8 +56,7 @@ class ImportList extends Component {
       id,
       name,
       enabled,
-      enableAuto,
-      minRefreshInterval
+      enableAuto
     } = this.props;
 
     return (
@@ -98,12 +96,6 @@ class ImportList extends Component {
           }
         </div>
 
-        <div className={styles.enabled}>
-          <Label kind={kinds.INFO} title='List Refresh Interval'>
-            {`${translate('Refresh')}: ${formatShortTimeSpan(minRefreshInterval)}`}
-          </Label>
-        </div>
-
         <EditImportListModalConnector
           id={id}
           isOpen={this.state.isEditImportListModalOpen}
@@ -130,7 +122,6 @@ ImportList.propTypes = {
   name: PropTypes.string.isRequired,
   enabled: PropTypes.bool.isRequired,
   enableAuto: PropTypes.bool.isRequired,
-  minRefreshInterval: PropTypes.string.isRequired,
   onConfirmDeleteImportList: PropTypes.func.isRequired
 };
 

frontend/src/Settings/ImportLists/Options/ImportListOptions.js
@@ -46,6 +46,25 @@ function ImportListOptions(props) {
         {
           hasSettings && !isFetching && !error &&
             <Form>
+              {/*
+              <FormGroup
+                advancedSettings={advancedSettings}
+                isAdvanced={true}
+              >
+                <FormLabel>{translate('ListUpdateInterval')}</FormLabel>
+
+                <FormInputGroup
+                  type={inputTypes.NUMBER}
+                  name="importListSyncInterval"
+                  min={10}
+                  unit="minutes"
+                  helpText={translate('ImportListSyncIntervalHelpText')}
+                  onChange={onInputChange}
+                  {...settings.importListSyncInterval}
+                />
+              </FormGroup>
+              */}
+
               <FormGroup
                 advancedSettings={advancedSettings}
                 isAdvanced={true}

src/NzbDrone.Core/Configuration/ConfigService.cs
@@ -116,6 +116,15 @@ namespace NzbDrone.Core.Configuration
             set { SetValue("AvailabilityDelay", value); }
         }
 
+        public int ImportListSyncInterval
+        {
+            get { return 4 * 60; } // Use hardcoded minutes value since the column was removed from the DB in PR #8573
+            set { }
+
+            // get { return GetValueInt("ImportListSyncInterval", 6); }
+            // set { SetValue("ImportListSyncInterval", value); }
+        }
+
         public string ListSyncLevel
         {
             get { return GetValue("ListSyncLevel", "disabled"); }

src/NzbDrone.Core/Configuration/IConfigService.cs
@@ -62,6 +62,7 @@ namespace NzbDrone.Core.Configuration
         bool AllowHardcodedSubs { get; set; }
         string WhitelistedHardcodedSubs { get; set; }
 
+        int ImportListSyncInterval { get; set; }
         string ListSyncLevel { get; set; }
         string ImportExclusions { get; set; }
 

src/NzbDrone.Core/ImportLists/FetchAndParseImportListService.cs
@@ -64,6 +64,8 @@ namespace NzbDrone.Core.ImportLists
             foreach (var importList in importLists)
             {
                 var importListLocal = importList;
+
+                /* Fix List sync interval logic to allow manual list refreshes - DK - Broken from PR #8573
                 var importListStatus = _importListStatusService.GetLastSyncListInfo(importListLocal.Definition.Id);
 
                 if (importListStatus.HasValue)
@@ -77,6 +79,7 @@ namespace NzbDrone.Core.ImportLists
                         continue;
                     }
                 }
+                */
 
                 _logger.ProgressInfo("Syncing Movies for Import List {0} ({1})", importList.Name, importListLocal.Definition.Name);
 

src/NzbDrone.Core/Jobs/TaskManager.cs
@@ -127,7 +127,7 @@ namespace NzbDrone.Core.Jobs
 
                     new ScheduledTask
                     {
-                        Interval = 5,
+                        Interval = GetImportListSyncInterval(),
                         TypeName = typeof(ImportListSyncCommand).FullName
                     },
 
@@ -233,6 +233,23 @@ namespace NzbDrone.Core.Jobs
             return interval;
         }
 
+        private int GetImportListSyncInterval()
+        {
+            var interval = _configService.ImportListSyncInterval;
+
+            if (interval <= 0)
+            {
+                return 0;
+            }
+
+            if (interval < 60)
+            {
+                return 60;
+            }
+
+            return interval;
+        }
+
         public void Handle(CommandExecutedEvent message)
         {
             var scheduledTask = _scheduledTaskRepository.All().SingleOrDefault(c => c.TypeName == message.Command.Body.GetType().FullName);
@@ -254,6 +271,9 @@ namespace NzbDrone.Core.Jobs
             var rss = _scheduledTaskRepository.GetDefinition(typeof(RssSyncCommand));
             rss.Interval = GetRssSyncInterval();
 
+            var importList = _scheduledTaskRepository.GetDefinition(typeof(ImportListSyncCommand));
+            importList.Interval = GetImportListSyncInterval();
+
             var backup = _scheduledTaskRepository.GetDefinition(typeof(BackupCommand));
             backup.Interval = GetBackupInterval();
 
@@ -263,9 +283,10 @@ namespace NzbDrone.Core.Jobs
             var downloadedMoviesScan = _scheduledTaskRepository.GetDefinition(typeof(DownloadedMoviesScanCommand));
             downloadedMoviesScan.Interval = GetDownloadedMoviesScanInterval();
 
-            _scheduledTaskRepository.UpdateMany(new List<ScheduledTask> { rss, refreshMonitoredDownloads, downloadedMoviesScan, backup });
+            _scheduledTaskRepository.UpdateMany(new List<ScheduledTask> { rss, importList, refreshMonitoredDownloads, downloadedMoviesScan, backup });
 
             _cache.Find(rss.TypeName).Interval = rss.Interval;
+            _cache.Find(importList.TypeName).Interval = importList.Interval;
             _cache.Find(backup.TypeName).Interval = backup.Interval;
             _cache.Find(refreshMonitoredDownloads.TypeName).Interval = refreshMonitoredDownloads.Interval;
             _cache.Find(downloadedMoviesScan.TypeName).Interval = downloadedMoviesScan.Interval;